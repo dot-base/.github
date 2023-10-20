abfd633fec0641ecbd4c634187c5c4dd092bb9b6
velramiir <justus@zeinert.dev>
2023-10-20 11:48:57 +0200

    fix: local copy of main required

--- scripts/create-conflict-branch.sh
+++ scripts/create-conflict-branch.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+git fetch
 git reset --hard
+git checkout main
+git checkout release
 git checkout -b $BRANCH_NAME
 # attempt to merge to check for conflicts
-git merge --no-commit --no-ff origin/main
+git merge --no-commit --no-ff main
 EXIT_STATUS=$?
 git merge --abort
 if [ $EXIT_STATUS -eq 0 ]
 then
     # zero exit status means there are no merge conflicts
     # otherwise we need to manually merge
-    git merge --ff-only origin/main
+    git merge --ff-only main
 fi
 git push --set-upstream origin $BRANCH_NAME
 echo "BRANCH_NAME=$BRANCH_NAME" >> $GITHUB_OUTPUT
