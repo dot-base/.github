2a16c7c08737479758f2e0e381a343aad3f06bc3
velramiir <justus@zeinert.dev>
2024-04-02 18:02:07 +0200

    Revert "feat: new changelog category for security"
    This reverts commit 90d469434eced65629458e054ac1fd01f8c0d903.

--- .github/changelog.config.js
+++ .github/changelog.config.js
@@ -5,7 +5,6 @@ module.exports = config({
   types: [
     { type: "feat", section: ":rocket: New Features" },
     { type: "fix", section: ":fire_extinguisher: Bugs" },
-    { type: "fix(sec)", section: ":oncoming_police_car: Security and Vulnerabilities" },
     { type: "chore", section: ":broom: Chore" },
     { type: "chore(deps)", section: ":nut_and_bolt: Dependencies" },
   ],
